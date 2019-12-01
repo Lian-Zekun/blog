88520d06e664512b9a4786f15d823185530a4e5e
lianzk <1534680930@qq.com>
2019-12-01 10:14:20 +0800

    更改setting文件

--- my_blog/settings/production.py
+++ my_blog/settings/production.py
@@ -1,5 +1,7 @@
 from .base import *
 
-SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+# SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+SECRET_KEY = '-)*3jy4h(5(1qx)(4v!y70dm6=@qg1xmcn0ny%uiq@20$&fqq('
 DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['.lianzekun.com', '127.0.0.1', 'localhost']
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
