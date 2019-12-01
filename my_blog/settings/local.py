5bbd92d58614c244ac46530b21068ba5ff3fd07f
lianzk <1534680930@qq.com>
2019-12-01 10:39:05 +0800

    更改setting文件

--- my_blog/settings/local.py
+++ my_blog/settings/local.py
@@ -3,4 +3,3 @@ from .base import *
 SECRET_KEY = '-)*3jy4h(5(1qx)(4v!y70dm6=@qg1xmcn0ny%uiq@20$&fqq('
 DEBUG = True
 ALLOWED_HOSTS = ['*']
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
--- my_blog/settings/production.py
+++ my_blog/settings/production.py
@@ -4,4 +4,3 @@ from .base import *
 SECRET_KEY = '-)*3jy4h(5(1qx)(4v!y70dm6=@qg1xmcn0ny%uiq@20$&fqq('
 DEBUG = False
 ALLOWED_HOSTS = ['.lianzekun.com', '127.0.0.1', 'localhost']
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
